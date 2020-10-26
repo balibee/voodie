43fce34692ef460bc327348e61bc16b46c013f29
balibee <r.boebner@protonmail.ch>
2020-10-26 08:41:26 +0100

    Add post form, clean up code

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -3,21 +3,15 @@ const express = require('express')
 const router = express.Router()
 
 const Foodie = require('../models/foodie')
-const Users = require('../models/users')
-
-const users = new Users()
-
 /* GET signup page. */
-router.get('/', function (req, res) {
+router.get('/', (req, res) => {
   res.render('signup', { title: 'Signup' })
 })
 
-// GET sign up input
-
-router.post('/', function (req, res) {
-  const foodie = new Foodie(req.body.name, req.body.email, req.body.password)
-  users.foodies.push(foodie)
-  res.redirect('/homepage')
+// POST sign up input
+router.post('/', async (req, res) => {
+  await Foodie.create({ name: req.body.name, email: req.body.email, password: req.body.password })
+  res.send('sucessfully signed up')
 })
 
 module.exports = router
