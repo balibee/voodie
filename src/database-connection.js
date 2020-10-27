21006dc80597e1fe0454bc9c910f71a7f29993c3
balibee <r.boebner@protonmail.ch>
2020-10-27 07:33:47 +0100

    add docker-compose

--- src/database-connection.js
+++ src/database-connection.js
@@ -3,12 +3,17 @@ const mongoose = require('mongoose')
 const username = process.env.MONGODB_USERNAME
 const password = process.env.MONGODB_PASSWORD
 const dbName = process.env.MONGODB_DATABASE
+let connectionString = process.env.MONGODB_CONNECTION_STRING
+
+if (!connectionString) {
+  connectionString = `mongodb+srv://${username}:${password}@cluster0.wb2fx.mongodb.net/${dbName}?retryWrites=true&w=majority`
+}
 
 mongoose.set('debug', true)
 mongoose.set('useCreateIndex', true)
 
 mongoose
-  .connect(`mongodb+srv://${username}:${password}@cluster0.wb2fx.mongodb.net/${dbName}?retryWrites=true&w=majority`, {
+  .connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
