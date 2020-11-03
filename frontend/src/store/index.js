7c5b66ec2ca80d6f0707eb9eec47d1f71c222322
balibee <r.boebner@protonmail.ch>
2020-11-03 15:43:51 +0100

    Add fetch requests for recipes and individual recipe

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
-  actions: {},
+  actions: {
+    async fetchRecipe(store, id) {
+      const recipeRequest = await axios.get(`/api/recipes/${id}`)
+      return recipeRequest.data
+    },
+    async fetchRecipes() {
+      const recipesRequest = await axios.get(`/api/recipes`)
+      return recipesRequest.data
+    }
+  },
   modules: {}
 })
