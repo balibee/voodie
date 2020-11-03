1e9eb4fe98391feddbc4555eb7f258398c0f6c4e
balibee <r.boebner@protonmail.ch>
2020-11-03 15:42:53 +0100

    Create inspiration vue

---
+++ frontend/src/views/inspiration.vue
@@ -0,0 +1,26 @@
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'Inspiration',
+  data() {
+    return {
+      recipes: []
+    }
+  },
+  async created() {
+    this.recipes = await this.fetchRecipes()
+  },
+  methods: {
+    ...mapActions(['fetchRecipes'])
+  }
+}
+</script>
+
+<template lang="pug">
+  .home
+    .container.text-center
+      h2 Get inspired with these delicious recipes!
+      div(v-for="recipe in recipes")
+        router-link(:to="`/inspiration/recipe/${recipe._id}`") {{ recipe.name }}
+</template>
