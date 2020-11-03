ee3267ad769f478988185c36f47b7fa8e465ffea
balibee <r.boebner@protonmail.ch>
2020-11-03 12:50:29 +0100

    Add description requirement to recipe model

--- backend/src/models/recipe.js
+++ backend/src/models/recipe.js
@@ -6,6 +6,10 @@ const recipeSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  description: {
+    type: [String],
+    required: true,
+  },
   ingredients: {
     type: [String],
     required: true,
