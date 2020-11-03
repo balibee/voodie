d1b59da1b3d56094d8ab6954f636ef326c2f4451
balibee <r.boebner@protonmail.ch>
2020-11-03 17:06:57 +0100

    Fix up recipes.vue

---
+++ frontend/src/views/recipes.vue
@@ -0,0 +1,26 @@
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'Recipes',
+  data() {
+    return {
+      recipes: []
+    }
+  },
+  async created() {
+    this.recipes = await this.fetchRecipes()
+  },
+  methods: {
+    ...mapActions(['fetchRecipes'])
+  }
+}
+</script>
+
+<template lang="pug">
+  .home
+    .container.text-center
+      h2 All Recipes
+      div(v-for="recipe in recipes")
+        router-link(:to="`/recipes/${recipe._id}`") {{ recipe.name }}
+</template>
