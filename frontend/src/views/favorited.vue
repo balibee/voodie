14b9672a86251efccea758c4b6960f1b122ef8c5
balibee <r.boebner@protonmail.ch>
2020-11-07 15:54:40 +0100

    Add favorited recipes vue

---
+++ frontend/src/views/favorited.vue
@@ -0,0 +1,28 @@
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'Favorites',
+  components: {},
+  data() {
+    return {
+      favoritedRecipes: []
+    }
+  },
+
+  async created() {
+    this.favoritedRecipes = await this.fetchFavoritedRecipes()
+  },
+
+  methods: {
+    ...mapActions(['fetchFavoritedRecipes'])
+  }
+}
+</script>
+
+<template lang="pug">
+  .container
+    h1 Favorited Recipes
+    router-link(:to="`/recipes/${recipe.id}`") {{ recipe.name }}
+    div (v-for='recipe in favoritedRecipes')
+</template>
