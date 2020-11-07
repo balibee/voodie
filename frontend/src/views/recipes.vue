b4e50ecc889fcf520865f56483d004161d4c6bf8
balibee <r.boebner@protonmail.ch>
2020-11-07 16:02:14 +0100

    Add placeholders in create recipe form

--- frontend/src/views/recipes.vue
+++ frontend/src/views/recipes.vue
@@ -27,16 +27,19 @@ export default {
         form(@submit.prevent='createRecipe(recipe)')
           .form-group
             label(for='name') Name
-            input.form-control#name(v-model='recipe.name' type='text' name='name')
+            input.form-control#name(v-model='recipe.name' type='text' name='name' placeholder='Write the name of your recipe')
           .form-group
             label(for='description') Description
-            input.form-control#description(v-model='recipe.description' type='text' name='decscription')
+            input.form-control#description(v-model='recipe.description' type='text' name='decscription' placeholder='Write about your recipe')
+          .form-group
+            label(for='tags') Tags
+            input.form-control#tags(v-model='recipe.tags' type='text' name='tags' placeholder='Separate tags by commas')
           .form-group
             label(for='ingredients') Ingredients
-            textarea.form-control#ingredients(v-model='recipe.ingredients' rows='4' name='ingredients')
+            textarea.form-control#ingredients(v-model='recipe.ingredients' rows='4' name='ingredients' placeholder='List your ingredients, each on a new line')
           .form-group
             label(for='instructions') Instructions
-            textarea.form-control#instructions(v-model='recipe.instructions' rows='4' name='instructions')
+            textarea.form-control#instructions(v-model='recipe.instructions' rows='4' name='instructions' placeholder='List the instructions, each on a new line')
           .form-row
             .form-group.col-2
               label.sr-only(for='cookTime') Cook Time
