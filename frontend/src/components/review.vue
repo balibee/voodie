b3d0720f1cd1c195668f22feb9a474dca9ea81e3
balibee <r.boebner@protonmail.ch>
2020-11-09 08:49:19 +0100

    Add review component

---
+++ frontend/src/components/review.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: 'Review',
+  props: ['review']
+}
+</script>
+
+<template lang="pug">
+  .row.review
+    .col-2
+      p {{ review.foodieId }}
+      //- img.rounded-circle.img-thumbnail.profile(src='https://i.pravatar.cc/300')
+    .col-2
+      p {{ review.rating }}
+    .col-8
+      p {{ review.review}}
+</template>
+
+<style lang="scss">
+.review {
+  align-items: center;
+  padding-bottom: 10px;
+}
+
+.profile {
+  padding: 0px;
+  max-height: 60px;
+  vertical-align: middle;
+}
+</style>
