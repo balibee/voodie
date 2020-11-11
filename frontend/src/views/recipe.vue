7ed217ac999425bc74cef7b7c211924e2958bb77
balibee <r.boebner@protonmail.ch>
2020-11-11 15:02:28 +0100

    Remove plus sign from recipe.vue

--- frontend/src/views/recipe.vue
+++ frontend/src/views/recipe.vue
@@ -54,9 +54,6 @@ export default {
     .container
       .row.recipe-page-image
         img.recipe-page.img-fluid(src='https://picsum.photos/600/300?random=1', alt='...')
-        svg.bi.bi-plus-circle(width='3em', height='3em', viewBox='0 0 16 16', fill='currentColor', xmlns='http://www.w3.org/2000/svg')
-          path(fill-rule='evenodd', d='M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z')
-          path(fill-rule='evenodd', d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z')
 
       .row.recipe-info
         h2.text-center.title {{ recipe.name }}
