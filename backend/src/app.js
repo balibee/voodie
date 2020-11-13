16d39e5cbed0d0c7bb7b784ebdd3305d686dcbf3
balibee <r.boebner@protonmail.ch>
2020-11-13 15:58:38 +0100

    Clean up function arrows

--- backend/src/app.js
+++ backend/src/app.js
@@ -73,12 +73,12 @@ app.use('/api/recipes', recipesRouter)
 app.use('/api/mealplanner', mealPlannerRouter)
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404))
 })
 
 // error handler
-app.use(function (err, req, res) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
