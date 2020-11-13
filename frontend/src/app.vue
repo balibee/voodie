1c191a03e90b877ac7c5d6b240e64814a919032b
balibee <r.boebner@protonmail.ch>
2020-11-13 16:03:52 +0100

    Add navbar to app.vue instead of own component

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,8 +1,4 @@
-<script type="text/javascript">
-import navbar from './components/navbar.vue'
 export default {
-  components: {
-    navbar
   }
 }
 </script>
@@ -15,6 +11,19 @@ export default {
 <template lang="pug">
   #app
     #navbar
-      navbar
+      nav.navbar.navbar-expand-md.navbar-light.bg-light
+        .container-fluid
+          a.navbar-brand Voodie
+          button.navbar-toggler(type='button' data-toggle='collapse' data-target='#navbarsExampleDefault' aria-controls='navbarsExampleDefault' aria-expanded='false' aria-label='Toggle navigation')
+            span.navbar-toggler-icon
+          #navbarsExampleDefault.collapse.navbar-collapse
+            ul.navbar-nav.mr-auto.mb-2.mb-md-0
+              li.nav-item
+                router-link.nav-link(to='/profile') Profile
+              li.nav-item
+                router-link.nav-link(to='/mealplanner') Meal Planner
+              li.nav-item
+                router-link.nav-link(to='/recipes') Recipes
+            a.btn.btn-outline-success(@click='doLogout' href="#") Logout
     router-view
 </template>
