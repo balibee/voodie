977b8c1d4478b9fd02df71089b3479707536f137
balibee <r.boebner@protonmail.ch>
2020-11-13 16:07:41 +0100

    Add login and register paths, also redirects when no user is logged in to router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,52 +1,76 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/home.vue'
+import Profile from '../views/profile.vue'
 import MealPlanner from '../views/mealplanner.vue'
 import Recipes from '../views/recipes.vue'
 import Favorited from '../views/favorited.vue'
+import Login from '../views/login.vue'
+import Register from '../views/register.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/mealplanner',
-    name: 'mealplanner',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: MealPlanner
-  },
-  // {
-  //   path: '/inspiration/',
-  //   name: 'inspiration',
-  //   component: () => import('../views/inspiration.vue')
-  // },
-  {
-    path: '/recipes/:id',
-    name: 'recipe detail',
-    component: () => import('../views/recipe.vue')
-  },
-  {
-    path: '/recipes/',
-    name: 'recipes',
-    component: Recipes
-  },
-  {
-    path: '/recipes/:id/favorites',
-    name: 'Favorited',
-    component: Favorited
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
-
-export default router
+export default function init(store) {
+  return new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+      {
+        path: '/',
+        name: 'Profile',
+        component: Profile
+      },
+      {
+        path: '/mealplanner',
+        name: 'mealplanner',
+        component: MealPlanner
+      },
+      // {
+      //   path: '/inspiration/',
+      //   name: 'inspiration',
+      //   component: () => import('../views/inspiration.vue')
+      // },
+      {
+        path: '/recipes/:id',
+        name: 'recipe detail',
+        component: () => import('../views/recipe.vue')
+      },
+      {
+        path: '/recipes',
+        name: 'recipes',
+        component: Recipes
+      },
+      {
+        path: '/recipes/:id/favorites',
+        name: 'Favorited',
+        component: Favorited
+      },
+      {
+        path: '/register',
+        name: 'register',
+        component: Register,
+        beforeEnter(to, from, next) {
+          if (store.state.foodie) return next('/profile')
+          return next()
+        }
+      },
+      {
+        path: '/login',
+        name: 'login',
+        component: Login,
+        beforeEnter(to, from, next) {
+          if (store.state.foodie) return next('/profile')
+          return next()
+        }
+      },
+      {
+        path: '/profile',
+        name: 'profile',
+        component: Profile,
+        beforeEnter(to, from, next) {
+          if (!store.state.foodie) return next('/login')
+          return next()
+        }
+      }
+    ]
+  })
+}
