37c2383a502073cfacf592aa24e74471f5d3624d
balibee <r.boebner@protonmail.ch>
2020-11-13 16:00:16 +0100

    Add account to backend routes

---
+++ backend/src/routes/account.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const passport = require('passport')
+
+const Foodie = require('../models/foodie')
+
+const router = express.Router()
+
+router.get('/session', (req, res) => {
+  res.send(req.foodie) // this was different in the notes (req.session)
+})
+
+router.post('/', async (req, res, next) => {
+  const { name, email, password } = req.body
+
+  try {
+    const foodie = await Foodie.register({ name, email }, password)
+    res.send(foodie)
+  } catch (e) {
+    next(e)
+  }
+})
+
+router.post('/session', passport.authenticate('local', { failWithError: true }), async (req, res) => {
+  res.send(req.foodie)
+})
+
+router.delete('/session', async (req, res, next) => {
+  await req.logout()
+
+  req.session.regenerate(err => {
+    if (err) return next(err)
+
+    return res.sendStatus(200)
+  })
+})
+
+module.exports = router
